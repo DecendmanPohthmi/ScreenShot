64075db728f6ce7c57ca12f9ac225db4204d0734
Decendman Pohthmi <decendmanp@gmail.com>
2025-03-19 21:04:47 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -2,7 +2,7 @@ document.getElementById('screenshot-btn').addEventListener('click', () => {
     console.log("Requesting screenshot...");
 
     // ✅ Open the download link in a new tab
-    window.open('https://screenshot-backend-6uxr.onrender.com/screenshot', '_blank');
+    window.open('http://localhost:3000/screenshot', '_blank');
 
     console.log("Screenshot requested!");
 });
